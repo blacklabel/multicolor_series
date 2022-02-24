fcc5ffc99a41def7e125d197720547bc054ee7e4
Paweł Fus <pawel.fus.88@gmail.com>
2022-02-24 11:01:07 +0100

    Bump packages, lint sources.

--- js/multicolor_series.js
+++ js/multicolor_series.js
@@ -655,5 +655,5 @@
 	
 	};
 	
-	H.seriesTypes.coloredarea.prototype.drawLegendSymbol = Highcharts._modules['Core/Legend/LegendSymbol.js'].drawRectangle;
+	H.seriesTypes.coloredarea.prototype.drawLegendSymbol = Highcharts._modules['Core/Legend/LegendSymbol.js'].drawRectangle; // eslint-disable-line
 }));
